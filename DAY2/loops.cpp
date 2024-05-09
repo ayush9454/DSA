1cada84182e38f244035a345c432a7453956dbf2
AYUSH <singhayush9454@gmail.com>
2024-05-10 02:27:40 +0530

    modified:   DAY2/loops.cpp 	modified:   DAY2/loops.exe 	new file:   DAY2/patterns.cpp 	new file:   DAY2/patterns.exe 	new file:   DAY2/patterns.txt

--- DAY2/loops.cpp
+++ DAY2/loops.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
-    for(int i=0; i<5;i++){
-        cout<< "love you\n";
+//   for(int i=100; i>0;i= i/2){
+//         cout<< i << endl;
+//     }
+    int n;
+    // if (cin>>n){
+    //     cout<< "Ayush";
+    // }
+    if (cout << n){
+        cout<< "ayush";
     }
 }
---
+++ DAY2/patterns.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int row=0;row<3;row++){
+        for(int column=0;column<5;column++){
+            cout << "*";
+        }
+        cout<<endl;
+    }
+}
