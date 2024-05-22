d45068effed326925c8c47eed86e39995964f501
AYUSH <singhayush9454@gmail.com>
2024-05-22 21:44:50 +0530

    22th May

---
+++ DAY4/Functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printname() {
+        int n;
+        cout<<"Enter the value of n ";
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<"Ayush singh"<<endl;
+        }
+    }
+
+
+int main(){
+    printname();
+}
